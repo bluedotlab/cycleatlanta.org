fd942ab6ed08d52c6f030aeefc956df30f15c7e9
David Bassendine <david@bluedotlab.org>
2013-11-16 13:10:22 -0500

    When adding a new marker, remove previous directions layers

--- map.js
+++ map.js
@@ -1,20 +1,15 @@
 mapping = {
-  waypoints: [
-    '100 cherokee avenue, atlanta ga',
-    'anatolia cafe, atlanta ga',
-  ],
+  waypoints: [],
+  routeLayer: {},
 
   initialize: function() {
-    map = L.map('map');
+    map = L.map('map').setView([33.754269, -84.387493], 13);
     var apiKey = 'd9c90387c1314bc49a6922344ab43bc1';
     L.tileLayer('http://{s}.tile.cloudmade.com/'+apiKey+'/997/256/{z}/{x}/{y}.png', {
       attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
       maxZoom: 18
     }).addTo(map);
-    map.locate({setView: true, maxZoom: 12});
-    map.on('locationfound', function(e) {
-      map.setZoom(13);
-    });
+console.log(map);
   },
 
   click: function() {
@@ -26,20 +21,29 @@ mapping = {
     dir.route({
         locations: mapping.waypoints
     }); 
-    map.addLayer(MQ.routing.routeLayer({
+    mapping.routeLayer = MQ.routing.routeLayer({
         directions: dir,
         fitBounds: true
-    }));
-  }
+    });
+    map.addLayer(mapping.routeLayer);
+  },
+
+  removeDirections: function() {
+    for (var layerid in mapping.routeLayer._layers) {
+      map.removeLayer(mapping.routeLayer._layers[layerid]);
+    }
+  },
 }
 if (typeof window.jQuery != "undefined") {
   jQuery(mapping.initialize);
   jQuery(mapping.click);
-  jQuery(mapping.addDirections);
 }
 
 function addWaypoint(e) {
   var position = {latLng: { lat: e.latlng.lat, lng: e.latlng.lng}};
   mapping.waypoints.push(position);
-  jQuery(mapping.addDirections);
+  if (mapping.waypoints.length > 1) {
+    jQuery(mapping.addDirections);
+    if (mapping.waypoints.length > 2) jQuery(mapping.removeDirections);
+  }
 }
