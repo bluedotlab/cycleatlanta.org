09ae3cc0de5c5dd0685aad80895897c34de2938a
David Bassendine <david@bluedotlab.org>
2013-11-16 10:40:20 -0500

    On click, store locations and recalculate route

--- map.js
+++ map.js
@@ -1,4 +1,9 @@
-map = {
+mapping = {
+  waypoints: [
+    '100 cherokee avenue, atlanta ga',
+    'anatolia cafe, atlanta ga',
+  ],
+
   initialize: function() {
     map = L.map('map');
     var apiKey = 'd9c90387c1314bc49a6922344ab43bc1';
@@ -12,13 +17,14 @@ map = {
     });
   },
 
+  click: function() {
+    map.on('click', addWaypoint);
+  },
+
   addDirections: function() {
     dir = MQ.routing.directions();
     dir.route({
-        locations: [
-            '1137 morley avenue, atlanta ga',
-            'anatolia cafe, atlanta ga',
-        ]
+        locations: mapping.waypoints
     }); 
     map.addLayer(MQ.routing.routeLayer({
         directions: dir,
@@ -27,6 +33,13 @@ map = {
   }
 }
 if (typeof window.jQuery != "undefined") {
-  jQuery(map.initialize);
-  jQuery(map.addDirections);
+  jQuery(mapping.initialize);
+  jQuery(mapping.click);
+  jQuery(mapping.addDirections);
+}
+
+function addWaypoint(e) {
+  var position = {latLng: { lat: e.latlng.lat, lng: e.latlng.lng}};
+  mapping.waypoints.push(position);
+  jQuery(mapping.addDirections);
 }
